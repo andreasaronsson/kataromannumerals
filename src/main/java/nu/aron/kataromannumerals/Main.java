0d3d687364a3e44c076d1e46b0b4e63845978040
Andreas Aronsson <aron@aron.nu>
2017-01-22 18:55:48 +0100

    Seems to work

--- src/main/java/nu/aron/kataromannumerals/Main.java
+++ src/main/java/nu/aron/kataromannumerals/Main.java
@@ -1,15 +1,47 @@
 package nu.aron.kataromannumerals;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javaslang.Function1;
+import javaslang.Function2;
+import javaslang.collection.CharSeq;
+import javaslang.collection.Stream;
 
 /**
  * Entry point
  */
 class Main {
-    static final Logger log = LoggerFactory.getLogger(Main.class);
+    Function2<Integer, String, String> charMultiple = (i, s) -> Stream.continually(s).take(i).mkString();
 
-    static String numeral(int i) {
-        return "";
+    Function1<Integer, String> mille = number -> charMultiple.apply(number % (1000 * 10) / 1000, "M");
+    Function1<Integer, String> centum = number -> charMultiple.apply(number % (100 * 10) / 100, "C");
+    Function1<Integer, String> decem = number -> charMultiple.apply(number % (10 * 10) / 10, "X");
+    Function1<Integer, String> unus = number -> charMultiple.apply(number % (1 * 10) / 1, "I");
+
+    Function1<String, String> reduceQuinque = s -> CharSeq.of(s).replace("IIIII", "V").toString();
+    Function1<String, String> reduceQuinquaginta = s -> CharSeq.of(s).replace("XXXXX", "L").toString();
+    Function1<String, String> reduceQuingenti = s -> CharSeq.of(s).replace("CCCCC", "D").toString();
+
+    Function1<String, String> reduceNovem = s -> CharSeq.of(s).replace("VIIII", "IX").toString();
+    Function1<String, String> reduceNonaginta = s -> CharSeq.of(s).replace("LXXXX", "XC").toString();
+    Function1<String, String> reduceNongentesimus = s -> CharSeq.of(s).replace("DCCCC", "CM").toString();
+
+    Function1<String, String> reduceQuattuor = s -> CharSeq.of(s).replace("IIII", "IV").toString();
+    Function1<String, String> reduceQuadraginta = s -> CharSeq.of(s).replace("XXXX", "XL").toString();
+
+    String numeral2(int number) {
+        return mille.apply(number)
+                .concat(reduceNongentesimus.apply(reduceQuingenti.apply(centum.apply(number))))
+                .concat(reduceQuadraginta.apply(reduceNonaginta.apply(reduceQuinquaginta.apply(decem.apply(number)))))
+                .concat(reduceQuattuor.apply(reduceNovem.apply(reduceQuinque.apply(unus.apply(number)))));
+    }
+
+    Function1<String, String> reduceHundreds = reduceQuingenti.andThen(reduceNongentesimus);
+    Function1<String, String> reduceTens = reduceQuinquaginta.andThen(reduceNonaginta).andThen(reduceQuadraginta);
+    Function1<String, String> reduceOnes = reduceQuinque.andThen(reduceNovem).andThen(reduceQuattuor);
+
+    String numeral(int number) {
+        return mille.apply(number)
+                .concat(reduceHundreds.apply(centum.apply(number)))
+                .concat(reduceTens.apply(decem.apply(number)))
+                .concat(reduceOnes.apply(unus.apply(number)));
     }
 }
--- src/test/java/nu/aron/kataromannumerals/MainTest.java
+++ src/test/java/nu/aron/kataromannumerals/MainTest.java
@@ -3,25 +3,104 @@ package nu.aron.kataromannumerals;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import nu.aron.kataromannumerals.Main;
+import javaslang.CheckedFunction1;
+import javaslang.Tuple;
+import javaslang.control.Option;
+import javaslang.test.Arbitrary;
+import javaslang.test.CheckResult;
+import javaslang.test.Property;
 
 public class MainTest {
 
-    @Test
-    public final void testOne() {
+    Main testee;
 
-        assertThat(Main.numeral(1), is("I"));
+    @Before
+    public void setUp() {
+        testee = new Main();
     }
 
     @Test
-    public final void testTen() {
-        assertThat(Main.numeral(10), is("X"));
+    public void multipleChars() {
+        assertThat(testee.charMultiple.apply(2, "M"), is("MM"));
+        assertThat(testee.charMultiple.apply(0, "C"), is(""));
     }
 
     @Test
-    public final void testSeven() {
-        assertThat(Main.numeral(7), is("VII"));
+    public void thousands() {
+        assertThat(testee.mille.apply(2000), is("MM"));
+    }
+
+    @Test
+    public final void testThrees() {
+        Arbitrary<Integer> numbers = Arbitrary.integer()
+                .filter(i -> i < 3000)
+                .filter(i -> i > 0)
+                .filter(i -> i % 10 == 3);
+        CheckedFunction1<Integer, Boolean> threeIsIII = i -> testee.numeral(i).endsWith("III");
+        CheckResult result = Property.def("threes")
+                .forAll(numbers)
+                .suchThat(threeIsIII)
+                .check();
+        Option<Tuple> sample = result.sample();
+        sample.stdout();
+        result.assertIsSatisfied();
+    }
+
+    @Test
+    public final void testFives() {
+        Arbitrary<Integer> numbers = Arbitrary.integer()
+                .filter(i -> i < 3000)
+                .filter(i -> i > 0)
+                .filter(i -> i % 10 == 5);
+        CheckedFunction1<Integer, Boolean> fiveIsV = i -> testee.numeral(i).endsWith("V");
+        CheckResult result = Property.def("fives")
+                .forAll(numbers)
+                .suchThat(fiveIsV)
+                .check();
+        Option<Tuple> sample = result.sample();
+        sample.stdout();
+        result.assertIsSatisfied();
+    }
+
+    @Test
+    public final void testOnes() {
+        Arbitrary<Integer> numbers = Arbitrary.integer()
+                .filter(i -> i < 3000)
+                .filter(i -> i > 0)
+                .filter(i -> i % 10 == 1);
+        CheckedFunction1<Integer, Boolean> oneIsI = i -> testee.numeral(i).endsWith("I");
+        CheckResult result = Property.def("ones")
+                .forAll(numbers)
+                .suchThat(oneIsI)
+                .check();
+        Option<Tuple> sample = result.sample();
+        sample.stdout();
+        result.assertIsSatisfied();
+    }
+
+    @Test
+    public void examples() {
+        assertThat(testee.numeral(1), is("I"));
+        assertThat(testee.numeral(2), is("II"));
+        assertThat(testee.numeral(3), is("III"));
+        assertThat(testee.numeral(4), is("IV"));
+        assertThat(testee.numeral(5), is("V"));
+        assertThat(testee.numeral(6), is("VI"));
+        assertThat(testee.numeral(7), is("VII"));
+        assertThat(testee.numeral(8), is("VIII"));
+        assertThat(testee.numeral(9), is("IX"));
+        assertThat(testee.numeral(10), is("X"));
+        assertThat(testee.numeral(19), is("XIX"));
+        assertThat(testee.numeral(49), is("XLIX"));
+        assertThat(testee.numeral(99), is("XCIX"));
+        assertThat(testee.numeral(309), is("CCCIX"));
+        assertThat(testee.numeral(527), is("DXXVII"));
+        assertThat(testee.numeral(789), is("DCCLXXXIX"));
+        assertThat(testee.numeral(1888), is("MDCCCLXXXVIII"));
+        assertThat(testee.numeral(1999), is("MCMXCIX"));
+        assertThat(testee.numeral(2001), is("MMI"));
     }
 }
